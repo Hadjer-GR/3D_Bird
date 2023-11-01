ee2e1885561801244963bb7f4b1f963e17fa440b
hadjer_gr <ghrabhadjer@gmail.com>
2023-11-02 00:29:37 +0100

    update desgin of the login page

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,7 +4,7 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+    <div class="container-login min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div>
             <Link href="/">
                 <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
@@ -18,3 +18,17 @@ import { Link } from '@inertiajs/vue3';
         </div>
     </div>
 </template>
+<style>
+
+.container-login{
+    background-image:url("../../../public/assets/img/back-1.jpg") !important;
+    background-position: center !important;
+    height: 100vh !important;
+    width: 100vw !important;
+    background-repeat: no-repeat !important;
+    overflow: hidden !important;
+    object-fit:cover;
+
+
+}
+</style>
